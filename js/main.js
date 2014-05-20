c36271f8fefe7017a4267441f646a3349a8c4c17
Ryan Jarvis <rjarvis1@cnm.edu>
2014-05-19 23:41:57 -0600

    almost

--- js/main.js
+++ js/main.js
@@ -1,10 +1,25 @@
 $(document).ready(function(){
+  //TODO: consider expanding size opf list item erather than bumping
+  //padding for smoother effect
 
+  //TODO: maybe think about creating array of image and array of background pics,
+  //correlate and change background of carousel container for each image change
   $('.headerLink').mouseover(function(){
-    $(this).animate({'padding-top': 0, 'padding-bottom': 10}, 200);
+    var width = $(window).width();
+    if(width > 779)
+    {
+      $(this).animate({'padding-top': 0, 'padding-bottom': 8}, 300);
+    }
   });
 
-  $('headerLink').mouseout(function(){
-    $(this).animate({'padding-top': 10, 'padding-bottom': 0}, 200);
+  $('.headerLink').mouseout(function(){
+    var width = $(window).width();
+    if(width > 779)
+ 	 {
+      if($(this).hasClass('headerLink'))
+      {
+   		 $(this).animate({'padding-top': 15, 'padding-bottom': 0}, 200);
+      }
+    }
   });
 });
