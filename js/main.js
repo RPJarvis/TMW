c0a4d3f63e9caf1d7f6dc9fc9b7f41fb4f4d8718
Ryan Jarvis <rjarvis1@cnm.edu>
2014-06-25 14:50:53 -0600

    add js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
+var width = $(window).width();
+
 $(document).ready(function(){
   $('.headerLink').mouseover(function(){
-    var width = $(window).width();
     if(width > 779)
     {
       $(this).animate({'padding-top': 0, 'padding-bottom': 8}, 300);
@@ -8,7 +9,6 @@ $(document).ready(function(){
   });
 
   $('.headerLink').mouseout(function(){
-    var width = $(window).width();
     if(width > 779)
  	 {
       if($(this).hasClass('headerLink'))
@@ -18,22 +18,24 @@ $(document).ready(function(){
     }
   });
 
-  //TODO:this is focusing in li item rather than link item maybe use li a active insterad of javascrizipt q
+  //Listen for resize event here and reinitialize scroll spy??
   $('#navbar').on('activate.bs.scrollspy', function(){
-    //should the ul below be a li
-    $(this).closest('ul').find('.selected').removeClass('selected');
-    $(this).addClass('selected');
+    //$(this).closest('ul').find('.selected').removeClass('selected');
+    //$(this).addClass('selected');
   });
 
-  //TODO: make link padding stick, link scrolling with links without clicks
   $('li a').click(function() {
+
     $('html, body').animate({
         scrollTop: $($.attr(this, 'href')).offset().top
     }, 500);
 
-    $(this).closest('ul').find('.selected').removeClass('selected');
-    $(this).addClass('selected');
+    if ($(window).width() <= 779) {
+        $(".navbar-toggle").click();
+    }
 
+    //$(this).closest('ul').find('.selected').removeClass('selected');
+    //$(this).addClass('selected');
     return false;
   });
 });
