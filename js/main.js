d24749df5024cc5d70e49ad1803fdcd8d5773a31
Ryan Jarvis <rjarvis1@cnm.edu>
2014-08-05 10:44:58 -0600

    ready for prod, paths changed

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
-var width = $(window).width();
-
 $(document).ready(function(){
   $('.headerLink').mouseover(function(){
+    var width = $(window).width();
     if(width > 779)
     {
       $(this).animate({'padding-top': 0, 'padding-bottom': 8}, 300);
@@ -9,23 +8,13 @@ $(document).ready(function(){
   });
 
   $('.headerLink').mouseout(function(){
-    if(width > 779)
- 	 {
-      if($(this).hasClass('headerLink'))
-      {
-   		 $(this).animate({'padding-top': 15, 'padding-bottom': 0}, 200);
+    var width = $(window).width();
+      if (width > 779 && $(this).hasClass('headerLink')) {
+          $(this).animate({'padding-top': 15, 'padding-bottom': 0}, 200);
       }
-    }
-  });
-
-  //Listen for resize event here and reinitialize scroll spy??
-  $('#navbar').on('activate.bs.scrollspy', function(){
-    //$(this).closest('ul').find('.selected').removeClass('selected');
-    //$(this).addClass('selected');
   });
 
   $('li a').click(function() {
-
     $('html, body').animate({
         scrollTop: $($.attr(this, 'href')).offset().top
     }, 500);
@@ -34,8 +23,10 @@ $(document).ready(function(){
         $(".navbar-toggle").click();
     }
 
-    //$(this).closest('ul').find('.selected').removeClass('selected');
-    //$(this).addClass('selected');
     return false;
+  });
+
+  $(window).resize(function(){
+      $('body').scrollspy('refresh');
   });
 });
